7bc956e6b6967b2e7edf891dbf5b15424675aa4b
William Pereira <billpereira@MacBook-Pro-de-William.local>
2022-12-06 06:13:34 -0300

    Python solution day 6

--- day_6/solution_1.py
+++ day_6/solution_1.py
@@ -1,15 +1,20 @@
-def find_first_four_unique_chars(packet):
-    limit = 14
+def find_first_unique_chars(packet, limit):
     for pos, i in enumerate(packet):
         verification_packet = packet[pos : pos + limit]
         if len([*set(verification_packet)]) == limit:
             return pos
 
 
-begin_of_packets = []
-with open("input") as communication_file:
-    for packet in communication_file:
-        # print(packet)
-        begin_of_packets.append(find_first_four_unique_chars(packet) + 14)
+def main():
+    limit = 14
+    begin_of_packets = []
+    with open("input") as communication_file:
+        for packet in communication_file:
+            begin_of_packets.append(
+                find_first_unique_chars(packet, limit) + limit
+            )
 
-print(begin_of_packets)
+    print(begin_of_packets)
+
+
+main()
